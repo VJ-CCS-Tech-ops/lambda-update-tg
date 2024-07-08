23cae5020951be7de3a78e1fc7cd9280cf1ead90
vijayragavan <vijay.ragavan@sita.aero>
2024-07-08 21:46:33 +0100

    updated with 2 new functions

--- tg-update.py
+++ tg-update.py
@@ -11,8 +11,8 @@ target_group_arn = 'your-target-group-arn'
 def get_all_ips(endpoint):
     # Retrieve all IP addresses associated with the endpoint
     addresses = socket.getaddrinfo(endpoint, None)
-    # Filter and return only IPv4 addresses
-    return [addr[4][0] for addr in addresses if addr[1] == socket.SOCK_STREAM]
+    # Filter and return only unique IPv4 addresses
+    return list(set(addr[4][0] for addr in addresses if addr[1] == socket.SOCK_STREAM))
 
 def get_target_group_targets(target_group_arn):
     response = elbv2_client.describe_target_health(TargetGroupArn=target_group_arn)
@@ -25,22 +25,27 @@ def register_new_targets(target_group_arn, ips):
     )
 
 def deregister_old_targets(target_group_arn, targets):
-    elbv2_client.deregister_targets(
-        TargetGroupArn=target_group_arn,
-        Targets=[{'Id': target} for target in targets]
-    )
+    for target in targets:
+        elbv2_client.deregister_targets(
+            TargetGroupArn=target_group_arn,
+            Targets=[{'Id': target}]
+        )
 
 def lambda_handler(event, context):
     current_ips = get_all_ips(rds_proxy_endpoint)
     current_targets = get_target_group_targets(target_group_arn)
     
-    # Determine which targets need to be deregistered
+    # Determine which targets need to be deregistered and registered
     targets_to_deregister = [target for target in current_targets if target not in current_ips]
+    targets_to_register = [ip for ip in current_ips if ip not in current_targets]
     
-    # Register new targets
+    # Deregister old targets
     if targets_to_deregister:
         deregister_old_targets(target_group_arn, targets_to_deregister)
-    register_new_targets(target_group_arn, current_ips)
+        
+    # Register new targets
+    if targets_to_register:
+        register_new_targets(target_group_arn, targets_to_register)
     
     return {
         'statusCode': 200,
